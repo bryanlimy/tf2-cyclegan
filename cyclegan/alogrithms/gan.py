fix identity loss. initalizer, normalization and activation as input args

cyclegan/alogrithms/gan.py
@@ -26,11 +26,11 @@ class GAN:
 
     self.cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
-    self.discriminator_loss_coefficient = 0.5
-    self.cycle_loss_coefficient = 10
+    self.alpha = hparams.alpha
+    self.beta = hparams.beta
 
     self.mixed_precision = hparams.mixed_precision
-    self.error = utils.mse if hparams.cycle_error == 'mse' else utils.mae
+    self.error_function = utils.get_error_function(hparams.error)
 
   def get_models(self):
     return self.G, self.F, self.X, self.Y
@@ -44,15 +44,14 @@ class GAN:
         tf.ones_like(discriminate_real), discriminate_real)
     fake_loss = self.cross_entropy(
         tf.zeros_like(discriminate_fake), discriminate_fake)
-    total_loss = real_loss + fake_loss
-    return self.discriminator_loss_coefficient * total_loss
+    return 0.5 * (real_loss + fake_loss)
 
   def cycle_loss(self, real, cycled):
-    return self.cycle_loss_coefficient * self.error(real, cycled)
+    return self.alpha * self.error_function(real, cycled)
 
   def identity_loss(self, real, identity):
-    """ Calculate the MAE(x, F(x)) or MAE(y, G(y))"""
-    return self.cycle_loss_coefficient * 0.5 * self.error(real, identity)
+    """ calculate identity loss || x - F(x) || or || y - G(y) || """
+    return self.beta * self.error_function(real, identity)
 
   @tf.function
   def cycle_step(self, x, y, training=False):
@@ -80,22 +79,21 @@ class GAN:
       G_loss = self.generator_loss(discriminate_fake_y)
       F_loss = self.generator_loss(discriminate_fake_x)
 
-      result.update({'G_loss': G_loss, 'F_loss': F_loss})
-
       cycle_loss = self.cycle_loss(x, cycled_x) + self.cycle_loss(y, cycled_y)
 
-      result.update({'cycle_loss': cycle_loss})
-
-      x_identity_loss = self.identity_loss(x, self.F(x, training=True))
-      y_identity_loss = self.identity_loss(y, self.G(y, training=True))
+      G_identity_loss = self.identity_loss(y, self.G(y, training=True))
+      F_identity_loss = self.identity_loss(x, self.F(x, training=True))
 
       result.update({
-          'x_identity_loss': x_identity_loss,
-          'y_identity_loss': y_identity_loss
+          'G_loss': G_loss,
+          'F_loss': F_loss,
+          'cycle_loss': cycle_loss,
+          'G_identity_loss': G_identity_loss,
+          'F_identity_loss': F_identity_loss
       })
 
-      G_loss += cycle_loss + x_identity_loss
-      F_loss += cycle_loss + y_identity_loss
+      G_loss += cycle_loss + G_identity_loss
+      F_loss += cycle_loss + F_identity_loss
 
       X_loss = self.discriminator_loss(discriminate_x, discriminate_fake_x)
       Y_loss = self.discriminator_loss(discriminate_y, discriminate_fake_y)
@@ -123,8 +121,8 @@ class GAN:
     same_y = self.G(y, training=False)
 
     return {
-        'MSE(X, F(G(X)))': utils.mse(x, cycled_x),
-        'MSE(Y, G(F(Y)))': utils.mse(y, cycled_y),
-        'MSE(X, F(X))': utils.mse(x, same_x),
-        'MSE(Y, G(Y))': utils.mse(y, same_y)
+        'MSE(X, F(G(X)))': utils.mean_square_error(x, cycled_x),
+        'MSE(Y, G(F(Y)))': utils.mean_square_error(y, cycled_y),
+        'MSE(X, F(X))': utils.mean_square_error(x, same_x),
+        'MSE(Y, G(Y))': utils.mean_square_error(y, same_y)
     }

cyclegan/alogrithms/utils.py
@@ -1,22 +1,22 @@
 import tensorflow as tf
 
 
-def mse(a, b):
+def mean_square_error(a, b):
   return tf.reduce_mean(tf.square(a - b))
 
 
-def mae(a, b):
+def mean_absolute_error(a, b):
   return tf.reduce_mean(tf.abs(a - b))
 
 
-def denormalize(x, x_min, x_max):
-  ''' re-scale signals back to its original range '''
-  return x * (x_max - x_min) + x_min
+def huber_loss(a, b):
+  return tf.reduce_mean(tf.losses.huber(a, b, delta=1.0))
 
 
-def update_dict(dict1, dict2):
-  """ Add content of dict2 to dict1 """
-  for key, value in dict2.items():
-    if key not in dict1:
-      dict1[key] = []
-    dict1[key].append(value)
+def get_error_function(name):
+  if name == 'mse':
+    return mean_square_error
+  elif name == 'mae':
+    return mean_absolute_error
+  elif name == 'huber':
+    return huber_loss

cyclegan/alogrithms/wgan_gp.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 from . import utils
 from .gan import GAN
-from .optimizer import Optimizer
+from ..utils import update_dict
 
 
 @register('wgan_gp')
@@ -30,22 +30,21 @@ class WGAN_GP(GAN):
       G_loss = self.generator_loss(discriminate_fake_y)
       F_loss = self.generator_loss(discriminate_fake_x)
 
-      result.update({'G_loss': G_loss, 'F_loss': F_loss})
-
       cycle_loss = self.cycle_loss(x, cycled_x) + self.cycle_loss(y, cycled_y)
 
-      result.update({'cycle_loss': cycle_loss})
-
-      x_identity_loss = self.identity_loss(x, self.F(x, training=True))
-      y_identity_loss = self.identity_loss(y, self.G(y, training=True))
+      G_identity_loss = self.identity_loss(y, self.G(y, training=True))
+      F_identity_loss = self.identity_loss(x, self.F(x, training=True))
 
       result.update({
-          'x_identity_loss': x_identity_loss,
-          'y_identity_loss': y_identity_loss
+          'G_loss': G_loss,
+          'F_loss': F_loss,
+          'cycle_loss': cycle_loss,
+          'G_identity_loss': G_identity_loss,
+          'F_identity_loss': F_identity_loss
       })
 
-      G_loss += cycle_loss + x_identity_loss
-      F_loss += cycle_loss + y_identity_loss
+      G_loss += cycle_loss + G_identity_loss
+      F_loss += cycle_loss + F_identity_loss
 
       if self.mixed_precision:
         G_loss = self.G_optimizer.get_scaled_loss(G_loss)
@@ -69,7 +68,7 @@ class WGAN_GP(GAN):
       discriminate_interpolated = discriminator(interpolated, training=training)
     gradient = tape.gradient(discriminate_interpolated, interpolated)
     norm = tf.norm(tf.reshape(gradient, shape=(gradient.shape[0], -1)), axis=1)
-    return utils.mse(norm, 1.0)
+    return utils.mean_square_error(norm, 1.0)
 
   def discriminator_loss(self, discriminate_real, discriminate_fake):
     real_loss = -tf.reduce_mean(discriminate_real)
@@ -120,8 +119,8 @@ class WGAN_GP(GAN):
     result = {}
     for i in range(self.critic_steps):
       discriminator_results = self._train_discriminators(x, y)
-      utils.update_dict(result, discriminator_results)
+      update_dict(result, discriminator_results)
     generator_result = self._train_generators(x, y)
-    utils.update_dict(result, generator_result)
+    update_dict(result, generator_result)
     # calculate averages for all items in result
     return {key: tf.reduce_mean(value) for key, value in result.items()}

cyclegan/models/unet.py
@@ -4,18 +4,18 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers
 
-from .utils import get_model_name, Activation
+from . import utils
 
 
 @register('unet')
 def get_unet(hparams, name=''):
-  g = generator(hparams, name=get_model_name(name, is_generator=True))
-  d = discriminator(hparams, name=get_model_name(name, is_generator=False))
+  g = generator(hparams, name=utils.get_model_name(name, is_generator=True))
+  d = discriminator(
+      hparams, name=utils.get_model_name(name, is_generator=False))
   return g, d
 
 
-def downsample(hparams, filters, kernel_size):
-  initializer = tf.random_normal_initializer(0., 0.02)
+def downsample(hparams, filters, kernel_size, initializer='glorot_uniform'):
   layer = tf.keras.Sequential()
   layer.add(
       layers.Conv2D(
@@ -25,14 +25,12 @@ def downsample(hparams, filters, kernel_size):
           padding='same',
           use_bias=False,
           kernel_initializer=initializer))
-  if hparams.layer_norm:
-    layer.add(layers.LayerNormalization())
-  layer.add(Activation(hparams.activation))
+  layer.add(utils.Normalization(hparams.normalizer))
+  layer.add(utils.Activation(hparams.activation))
   return layer
 
 
-def upsample(hparams, filters, kernel_size):
-  initializer = tf.random_normal_initializer(0., 0.02)
+def upsample(hparams, filters, kernel_size, initializer='glorot_uniform'):
   layer = tf.keras.Sequential()
   layer.add(
       layers.Conv2DTranspose(
@@ -42,41 +40,43 @@ def upsample(hparams, filters, kernel_size):
           padding='same',
           use_bias=False,
           kernel_initializer=initializer))
-  layer.add(layers.LayerNormalization())
+  layer.add(utils.Normalization(hparams.normalizer))
+  layer.add(utils.Activation(hparams.activation))
   if hparams.dropout > 0:
     layer.add(layers.Dropout(hparams.dropout))
-  layer.add(Activation(hparams.activation))
   return layer
 
 
 def generator(hparams, name='generator'):
+  initializer = utils.Initializer(hparams.initializer)
+
   inputs = tf.keras.Input(shape=hparams.image_shape, name='inputs')
   outputs = inputs
 
   down_stack = [
-      downsample(hparams, 64, 4),
-      downsample(hparams, 128, 4),
-      downsample(hparams, 256, 4),
-      downsample(hparams, 512, 4),
-      downsample(hparams, 512, 4),
-      downsample(hparams, 512, 4),
-      downsample(hparams, 512, 4),
-      downsample(hparams, 512, 4),
+      downsample(hparams, 64, 4, initializer=initializer),
+      downsample(hparams, 128, 4, initializer=initializer),
+      downsample(hparams, 256, 4, initializer=initializer),
+      downsample(hparams, 512, 4, initializer=initializer),
+      downsample(hparams, 512, 4, initializer=initializer),
+      downsample(hparams, 512, 4, initializer=initializer),
+      downsample(hparams, 512, 4, initializer=initializer),
+      downsample(hparams, 512, 4, initializer=initializer),
   ]
 
   up_stack = [
-      upsample(hparams, 512, 4),
-      upsample(hparams, 512, 4),
-      upsample(hparams, 512, 4),
-      upsample(hparams, 512, 4),
-      upsample(hparams, 256, 4),
-      upsample(hparams, 128, 4),
-      upsample(hparams, 64, 4),
+      upsample(hparams, 512, 4, initializer=initializer),
+      upsample(hparams, 512, 4, initializer=initializer),
+      upsample(hparams, 512, 4, initializer=initializer),
+      upsample(hparams, 512, 4, initializer=initializer),
+      upsample(hparams, 256, 4, initializer=initializer),
+      upsample(hparams, 128, 4, initializer=initializer),
+      upsample(hparams, 64, 4, initializer=initializer),
   ]
 
   concat = tf.keras.layers.Concatenate()
 
-  # Downsampling through the model
+  # down sample through the model
   skips = []
   for down in down_stack:
     outputs = down(outputs)
@@ -84,7 +84,7 @@ def generator(hparams, name='generator'):
 
   skips = reversed(skips[:-1])
 
-  # Upsampling and establishing the skip connections
+  # up sample and establishing the skip connections
   for up, skip in zip(up_stack, skips):
     outputs = up(outputs)
     outputs = concat([outputs, skip])
@@ -94,10 +94,9 @@ def generator(hparams, name='generator'):
       kernel_size=4,
       strides=2,
       padding='same',
-      kernel_initializer=tf.random_normal_initializer(0., 0.02),
-      activation='tanh')(outputs)
+      kernel_initializer=initializer)(outputs)
 
-  outputs = layers.Activation('linear', dtype=tf.float32)(outputs)
+  outputs = layers.Activation('tanh', dtype=tf.float32)(outputs)
 
   return tf.keras.Model(inputs=inputs, outputs=outputs, name=name)
 
@@ -105,12 +104,12 @@ def generator(hparams, name='generator'):
 def discriminator(hparams, name='discriminator'):
   inputs = tf.keras.Input(hparams.image_shape, name='signals')
 
-  initializer = tf.random_normal_initializer(0., 0.02)
+  initializer = utils.Initializer(hparams.initializer)
 
-  outputs = downsample(hparams, 64, 4)(inputs)
-  outputs = downsample(hparams, 128, 4)(outputs)
-  outputs = downsample(hparams, 256, 4)(outputs)
-  outputs = downsample(hparams, 512, 4)(outputs)
+  outputs = downsample(hparams, 64, 4, initializer=initializer)(inputs)
+  outputs = downsample(hparams, 128, 4, initializer=initializer)(outputs)
+  outputs = downsample(hparams, 256, 4, initializer=initializer)(outputs)
+  outputs = downsample(hparams, 512, 4, initializer=initializer)(outputs)
 
   outputs = layers.ZeroPadding2D()(outputs)
   outputs = layers.Conv2D(
@@ -119,13 +118,9 @@ def discriminator(hparams, name='discriminator'):
       strides=1,
       use_bias=False,
       kernel_initializer=initializer)(outputs)
-
-  if hparams.layer_norm:
-    outputs = layers.LayerNormalization()(outputs)
-
-  outputs = Activation(hparams.activation)(outputs)
+  outputs = utils.Normalization(hparams.normalizer)(outputs)
+  outputs = utils.Activation(hparams.activation)(outputs)
   outputs = layers.ZeroPadding2D()(outputs)
-
   outputs = layers.Conv2D(
       filters=1, kernel_size=4, strides=1,
       kernel_initializer=initializer)(outputs)

cyclegan/models/utils.py
@@ -2,12 +2,30 @@ import io
 import os
 import numpy as np
 import tensorflow as tf
+import tensorflow_addons as tfa
 from tensorflow.keras import layers
 
 
 def Activation(name, **kwargs):
-  return layers.LeakyReLU(
-      **kwargs) if name == 'lrelu' else layers.Activation(name, **kwargs)
+  if name == 'lrelu':
+    return layers.LeakyReLU()
+  else:
+    return layers.Activation(name, **kwargs)
+
+
+def Normalization(name, **kwargs):
+  if name == 'layer_norm':
+    return layers.LayerNormalization(**kwargs)
+  elif name == 'batch_norm':
+    return layers.BatchNormalization(**kwargs)
+  elif name == 'instance_norm':
+    return tfa.layers.InstanceNormalization()
+  else:
+    return layers.Activation('linear')
+
+
+def Initializer(name):
+  return tf.keras.initializers.get(name)
 
 
 class Conv1DTranspose(layers.Layer):
@@ -34,7 +52,7 @@ class Conv1DTranspose(layers.Layer):
         kernel_initializer=kernel_initializer,
     )
 
-  def call(self, inputs):
+  def call(self, inputs, **kwargs):
     outputs = tf.expand_dims(inputs, axis=2)
     outputs = self.conv2dtranspose(outputs)
     outputs = tf.squeeze(outputs, axis=2)

cyclegan/utils/dataset_helper.py
@@ -51,7 +51,8 @@ def prepare_dataset(hparams, ds, train=False):
   return ds
 
 
-cardinality = tf.data.experimental.cardinality
+def cardinality(ds):
+  return tf.data.experimental.cardinality(ds)
 
 
 def get_datasets(hparams):

main.py
@@ -1,8 +1,10 @@
+import os
 import argparse
 import numpy as np
 from tqdm import tqdm
 from time import time
 import tensorflow as tf
+from shutil import rmtree
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 from cyclegan.utils import utils
@@ -22,43 +24,37 @@ def set_precision_policy(hparams):
   return policy
 
 
-def train(hparams, x_ds, y_ds, gan, summary):
+def train(hparams, x_ds, y_ds, gan, summary, epoch):
+  metrics = {}
   for x, y in tqdm(
       tf.data.Dataset.zip((x_ds, y_ds)),
       desc='Train',
       total=hparams.train_steps):
-    loss = gan.train(x, y)
+    result = gan.train(x, y)
     hparams.global_step += 1
-
-    if hparams.global_step % 100 == 0:
-      for key, value in loss.items():
-        summary.scalar(f'loss/{key}', value, step=hparams.global_step)
+    utils.update_dict(metrics, result, replace=False)
+  for key, value in metrics.items():
+    summary.scalar(f'loss/{key}', tf.reduce_mean(value), epoch, training=True)
 
 
 def validate(hparams, x_ds, y_ds, gan, summary, epoch):
-  metrics = {
-      'MSE(X, F(G(X)))': [],
-      'MSE(Y, G(F(Y)))': [],
-      'MSE(X, F(X))': [],
-      'MSE(Y, G(Y))': []
-  }
-
+  metrics = {}
   for x, y in tqdm(
       tf.data.Dataset.zip((x_ds, y_ds)),
       desc='Validation',
       total=hparams.validation_steps):
     result = gan.validate(x, y)
-    metrics = {key: metrics[key] + [value] for key, value in result.items()}
-
+    utils.update_dict(metrics, result, replace=False)
   metrics = {key: np.mean(value) for key, value in metrics.items()}
-
   for key, value in metrics.items():
     summary.scalar(f'mse/{key}', value, epoch, training=False)
-
   return metrics
 
 
 def main(hparams):
+  if hparams.clear_output_dir and os.path.exists(hparams.output_dir):
+    rmtree(hparams.output_dir)
+
   tf.keras.backend.clear_session()
 
   set_precision_policy(hparams)
@@ -76,7 +72,7 @@ def main(hparams):
     print('Epoch {:03d}/{:03d}'.format(epoch + 1, hparams.epochs))
 
     start = time()
-    train(hparams, x_train, y_train, gan, summary)
+    train(hparams, x_train, y_train, gan, summary, epoch)
     metrics = validate(hparams, x_validation, y_validation, gan, summary, epoch)
     end = time()
     summary.scalar('elapse', end - start, step=epoch)
@@ -98,22 +94,35 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument('--output_dir', default='runs')
   parser.add_argument('--algorithm', default='gan', type=str)
-  parser.add_argument('--model', default='mlp', type=str)
+  parser.add_argument('--model', default='unet', type=str)
   parser.add_argument('--num_units', default=32, type=int)
   parser.add_argument('--kernel_size', default=24, type=int)
   parser.add_argument('--strides', default=2, type=int)
-  parser.add_argument('--activation', default='lrelu', type=str)
+  parser.add_argument('--activation', default='elu', type=str)
   parser.add_argument('--dropout', default=0.2, type=float)
-  parser.add_argument('--layer_norm', action='store_true')
+  parser.add_argument(
+      '--normalizer',
+      default='layer_norm',
+      choices=[None, 'layer_norm', 'batch_norm', 'instance_norm'])
+  parser.add_argument('--initializer', default='glorot_uniform', type=str)
+  parser.add_argument(
+      '--alpha', default=10., type=float, help='cycle loss coefficient')
+  parser.add_argument(
+      '--beta', default=5., type=float, help='identity loss coefficient')
   parser.add_argument('--patch_gan', action='store_true')
   parser.add_argument('--epochs', default=200, type=int)
   parser.add_argument('--batch_size', default=32, type=int)
   parser.add_argument('--learning_rate', default=2e-4, type=float)
   parser.add_argument('--critic_steps', default=5, type=int)
   parser.add_argument('--gradient_penalty', default=10.0, type=float)
-  parser.add_argument('--cycle_error', default='mse', choices=['mse', 'mae'])
+  parser.add_argument(
+      '--error',
+      default='mae',
+      choices=['mse', 'mae', 'huber'],
+      help='error function to measure cycle loss and identity loss')
   parser.add_argument('--mixed_precision', action='store_true')
   parser.add_argument('--verbose', default=1, type=int)
+  parser.add_argument('--clear_output_dir', action='store_true')
   params = parser.parse_args()
 
   np.random.seed(1234)